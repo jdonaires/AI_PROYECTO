4a4e239bc4ebc92897608f4ff9f4c49e10af8c77
karlacampos7551 <karla_aries_97@hotmail.com>
2018-09-04 22:31:16 -0500

    cambio de mensaje
    es mas entendible

--- DAL/DABACCES.php
+++ DAL/DABACCES.php
@@ -1,7 +1,7 @@
 <?php
 // Conectando, seleccionando la base de datos
 $link = mysql_connect('mysql_host', 'mysql_user', 'mysql_password')
-    or die('No se pudo conectar: ' . mysql_error());
+    or die('nada : ' . mysql_error());
 echo 'Connected successfully';
 mysql_select_db('my_database') or die('No se pudo seleccionar la base de datos');
 
